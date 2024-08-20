3361c2129f6bc8f44fe97f531c37ba32364d3006
Okanekatsuro <maximov.roman@outlook.com>
2024-08-20 21:34:11 +0300

    убрал лишний грид и исправил мобильную версию

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,27 +1,28 @@
 import { GlareCard } from "./ui/glare-card";
 
-
 export function GlareCardDemo() {
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-6 md:gap-8 p-4">
-      <GlareCard className="flex flex-col items-center justify-center">
-      <p className="font-bold text-white text-4xl">3000 м²</p>
-        <p className="font-normal text-center text-neutral-100 mt-4 mx-2">
-        Гибкие условия аренды и возможность индивидуальной планировки позволят вам создать идеальное пространство для успешного ведения бизнеса. 
-        </p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <img
-          className="h-full w-full absolute inset-0 object-cover"
-          src="Foto5.jpg"
-        />
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-      <p className="font-bold text-white text-4xl text-center">100 000 <br/> человек в месяц</p>
-        <p className="font-normal text-center text-neutral-100 mt-4 mx-2">
-        Это отличная возможность для вашего бизнеса привлечь большое количество потенциальных клиентов. Площадь находится в самом центре торгового потока, обеспечивая высокую видимость и доступность. 
-        </p>
-      </GlareCard>
-    </div>
-  );
+    return (
+        <div className="flex flex-col lg:flex-row gap-8 p-4 items-center justify-center">
+            <GlareCard className="flex flex-col items-center justify-center">
+                <p className="font-bold text-white text-4xl">3000 м²</p>
+                <p className="font-normal text-center text-neutral-100 mt-4 mx-2">
+                    Гибкие условия аренды и возможность индивидуальной планировки позволят вам создать идеальное пространство для успешного ведения бизнеса.
+                </p>
+            </GlareCard>
+            <GlareCard className="flex flex-col items-center justify-center">
+                <img
+                    className="h-full w-full absolute inset-0 object-cover"
+                    src="Foto5.jpg"
+                />
+            </GlareCard>
+            <GlareCard className="flex flex-col items-center justify-center">
+                <p className="font-bold text-white text-4xl text-center">
+                    100 000 <br /> человек в месяц
+                </p>
+                <p className="font-normal text-center text-neutral-100 mt-4 mx-2">
+                    Это отличная возможность для вашего бизнеса привлечь большое количество потенциальных клиентов. Площадь находится в самом центре торгового потока, обеспечивая высокую видимость и доступность.
+                </p>
+            </GlareCard>
+        </div>
+    );
 }
--- src/components/sectioncards.tsx
+++ src/components/sectioncards.tsx
@@ -2,10 +2,10 @@ import { GlareCardDemo } from "./cards";
 
 export function SetionGlareCardDemo() {
     return (
-        <section className="relative flex justify-center items-center bg-white h-auto z-10 p-4 sm:p-6 lg:p-10">
-            <div className="grid gap-8 mx-auto max-w-full px-4">
+        <section className="relative flex items-center justify-center bg-white h-auto z-10 p-4 sm:p-6 lg:p-10">
+
                 <GlareCardDemo />
-            </div>
+
         </section>
     );
 }
